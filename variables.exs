1f37d320ab774e14a585b38c37ddf4014e05c792
Kofi Junior Eshun <eshunkofi23@gmail.com>
2025-05-03 23:12:55 -0400

    First Commit by Kofi on 5/03/2025 at 11:12 PM

---
+++ variables.exs
@@ -0,0 +1,9 @@
+# Variables in eleixir
+first_name = "Alice"
+last_name = "Smith"
+full_name = first_name <> " " <> last_name
+age = 30
+favorite_color = "blue"
+# TODO: Define a new variable 'favorite_color' and set it to "blue"
+# TODO: Modify the IO.puts statement to include "and likes <color>" at the end of the print statement
+IO.puts "The user #{full_name} is #{age} years old and has #{favorite_color} as favorite color."
